0ecd869ede336fcdf8f12e260b04dc4664396630
v_rusenli <v_rusenli@tencent.com>
2020-01-08 11:13:18 +0800

    临时保存

---
+++ 流畅的python/1.py
@@ -0,0 +1,15 @@
+
+
+# 装饰器通常会把函数替换成另一个函数
+
+def deco(func):
+    def inner():
+        print("running inner()")
+    return inner
+
+@deco
+def target():
+    print("running target()")
+
+target()
+print(target)
