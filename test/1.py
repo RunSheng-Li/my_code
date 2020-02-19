98141bf2431b261c9619c80dc07a499cbe2197df
v_rusenli <v_rusenli@tencent.com>
2020-02-19 11:59:32 +0800

    git

--- test/1.py
+++ test/1.py
@@ -1,1 +1,15 @@
-a = {}
+# dic1 = {"sam": ""}
+# print(dic1.get("sam", "") is True)
+#
+#
+# if None:
+#     print("aaa")
+
+
+# dict1 = {'a': 'b', 'c': 'd'}
+# dict2 = {'1': '2'}
+# dict1.update(dict2)
+# print(dict1)
+
+list1=["a","a","a"]
+print(list1.get("a"))
